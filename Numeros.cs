b2bfd891ba9a31f1adfec3c305f7c3e339b8f199
Francisco-Meza <86315912+Francisco-Meza@users.noreply.github.com>
2021-06-22 13:50:19 -0600

    SE AGREGO EL PROYECTO
    SE AGREGO EL EJERCICIO 2

---
+++ Numeros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Numeros
+{
+    int[] datos;
+    public Numeros(int[] datos)
+    {
+        Datos = datos;
+    }
+    public int[] Solucion()
+    {
+        Array.Sort(Datos);
+        return Datos;
+    }
+    public int[] Datos { get => datos; set => datos = value; }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] datos = new int[20];
+            Console.WriteLine("Ingresa 20 Numeros");
+            for(int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("ingresa el numero "+(i+1)+": ");
+                datos[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            string ans = "";
+            Numeros numeros = new Numeros(datos);
+            for(int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"{datos[i]}      {numeros.Solucion()[i]}\n");
+            }
+        }
+    }
+}
